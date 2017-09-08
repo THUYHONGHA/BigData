bc802b242ec8f8448f9f20de612b0db9e7278cd0
Thuy Hong Ha <thuyhong.ip@gmail.com>
2017-09-08 07:07:45 -0500

    HW4

---
+++ W1D4/src/MyComparator.java
@@ -0,0 +1,15 @@
+
+import java.util.Comparator;
+
+public class  MyComparator  implements Comparator {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		Pair p1 = (Pair) o1;
+		Pair p2 = (Pair) o2;
+		
+	   return  p1.getKey().compareTo(p2.getKey());
+
+	  }
+
+}
